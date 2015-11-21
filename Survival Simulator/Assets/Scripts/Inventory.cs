2e430f3982ee7d4c484d9a51ce2ae89d6ba98689
jdurantt0 <jdurantt0@gmail.com>
2015-11-20 21:04:48 -0500

    test

--- Survival Simulator/Assets/Scripts/Inventory.cs
+++ Survival Simulator/Assets/Scripts/Inventory.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
 
     public int wood;
     public int berries;
     public int rock;
+	private Text berriesText;
+	private Text woodText;
+	private Text rockText;
+
     private RayCast rayCast;
 
     void Awake()
     {
         rayCast = GameObject.Find("Main Camera").GetComponent<RayCast>();
+		woodText = GameObject.Find ("Wood").GetComponent<Text> ();
+		berriesText = GameObject.Find ("Berries").GetComponent<Text> ();
+		rockText = GameObject.Find ("Rock").GetComponent<Text> ();
     }
 
-    void Start ()
+	void Start() {
+
+	}
+
+    void Update ()
     {
-        wood = wood + rayCast.saveWood;
-        berries = berries + rayCast.saveBerries;
-        rock = rock + rayCast.saveRock;
+		woodText.text = wood.ToString ();
+		berriesText.text = berries.ToString ();
+		rockText.text = rock.ToString ();
     }
 }
---
+++ Survival Simulator/Assets/Scripts/TabMenuControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TabMenuControl : MonoBehaviour {
+
+	private GameObject tabMenu;
+
+	void Awake() {
+		tabMenu = GameObject.Find ("TabMenu");
+	}
+
+	void Start () {
+		tabMenu.SetActive (false);
+	}
+
+	void Update () {
+		if (!PauseScreen.isPaused) {
+			if (Input.GetKeyDown (KeyCode.Tab) && tabMenu.activeSelf == false) {
+				tabMenu.SetActive (true);
+			} else if (Input.GetKeyDown (KeyCode.Tab) && tabMenu.activeSelf == true) {
+				tabMenu.SetActive (false);
+			}
+		}
+		if (PauseScreen.isPaused && tabMenu.activeSelf == true) {
+			tabMenu.SetActive(false);
+		}
+	}
+}
